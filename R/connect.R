7385546fe9da408273be1af439eefca5b1ad2df8
Mischa ter Smitten <mischa@tersmitten.nl>
2020-02-18 13:19:21 +0100

    Fix for OS X not finding .my.cnf

--- R/connect.R
+++ R/connect.R
@@ -4,6 +4,6 @@
 #' @param default.file A MySQL configuration file, containing login credentials.
 #' @return A database connection (an object of class MySQLConnection).
 #' @export
-connect <- function(dbname = "oefenweb_nl_app", default.file = paste0("~", "/", ".my.cnf")) {
+connect <- function(dbname = "oefenweb_nl_app", default.file = path.expand(file.path("~", ".my.cnf"))) {
   return(DBI::dbConnect(RMySQL::MySQL(), dbname = dbname, default.file = default.file))
 }
